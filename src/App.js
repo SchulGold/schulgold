1ce6c59310b6bbb05a882c6e2033f15b277947de
mirunaen <mirunaen@gmail.com>
2020-05-01 19:57:03 +0200

    added dinamic questions

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Question from './components/Question';
+import questionsData from './data/questionsData';
+
 
 function App() {
   return (
     <div className="App">
-      <Question />
+      <Question data={questionsData} />
     </div>
   );
 }
