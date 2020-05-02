e09948921cc93f7802502c4dae7455c864bccb94
mirunaen <mirunaen@gmail.com>
2020-05-02 19:51:40 +0200

    added next function and next extra question

--- src/App.js
+++ src/App.js
@@ -5,12 +5,26 @@ import Question from './components/Question';
 import questionsData from './data/questionsData';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Question data={questionsData} />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      questionNumber: 0
+    }
+    this.nextQuestion = this.nextQuestion.bind(this)
+  }
+  nextQuestion() {
+    this.setState({ questionNumber: this.state.questionNumber + 1 })
+  }
+  render() {
+
+    return (
+      <div className="App">
+        <Question data={questionsData[this.state.questionNumber]} next={this.nextQuestion} />
+
+      </div>
+    );
+  }
 }
 
 export default App;
