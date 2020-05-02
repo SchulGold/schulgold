bc0f965c69ceb09aaf966f9113169a643574616c
bogdanned <bogdan.nedelcu@lokallead.de>
2020-05-02 16:51:55 +0200

    added login, storage and graphQL api

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 import Question from './components/Question';
 import questionsData from './data/questionsData';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+
+import { API, graphqlOperation } from 'aws-amplify';
+import * as queries from './graphql/queries';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Question data={questionsData} />
-    </div>
-  );
+
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+class App extends React.Component {
+  async componentDidMount() {
+    // Simple query
+    const allTodos = await API.graphql(graphqlOperation(queries.listMyTypes));
+    console.log(allTodos);
+  }
+
+
+  // Query using a parameter
+  // const oneTodo = await API.graphql(graphqlOperation(queries.getTodo, { id: 'some id' }));
+  // console.log(oneTodo);
+  render() {
+    return (
+      <div className="App" >
+        <Question data={questionsData} />
+        <AmplifySignOut />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App);
